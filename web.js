41bdf0b0d2cbfad630c420bbdd54622e61749d5d
Scott Duda <sduda84@gmail.com>
2013-07-14 23:31:36 -0400

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var index = fs.readFileSync('index.html');
+
+var indexString = index.toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(indexString);
 });
 
 var port = process.env.PORT || 5000;
